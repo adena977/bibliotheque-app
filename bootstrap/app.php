a141055517a2c13853daf76af82b0855f7e2f1ef
adena977 <adena977@gmail.com>
2026-05-20 02:14:28 +0300

    Fix ForceHttps import

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckRole;
 use App\Http\Middleware\RedirectIfAuthenticated;
+use Illuminate\Http\Middleware\ForceHttps;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+    $middleware->append(\Illuminate\Http\Middleware\ForceHttps::class);
         $middleware->alias([
             'role' => CheckRole::class,
             'guest' => RedirectIfAuthenticated::class,
